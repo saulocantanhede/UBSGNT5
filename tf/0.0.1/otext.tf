038aa8775fc54064daff4fd37e8b603d08068545
Saulo de Oliveira Cantanhêde <121267518+saulocantanhede@users.noreply.github.com>
2025-06-15 19:29:11 -0400

    Initial version

---
+++ tf/0.0.1/otext.tf
@@ -0,0 +1,7 @@
+@config
+@fmt:text-orig-full={surface}
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@writtenBy=Text-Fabric
+@dateWritten=2025-06-15T23:17:26Z
+
---
+++ tf/0.0.1/otype.tf
@@ -0,0 +1,17 @@
+@node
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2025-06-15T23:17:19Z
+
+1-138013	word
+138014-138040	book
+138041-138300	chapter
+138301-194557	clause
+194558-270709	functionalphrase
+270710-271567	functionalword
+271568-279508	parsedtext
+279509-422610	phrase
+422611-423478	section
+423479-431419	sentence
+431420-439360	tree
+439361-447301	verse
