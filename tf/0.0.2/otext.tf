e542fffd7ce1b0a1a20049c50bb0497a12d53127
Saulo de Oliveira Cantanhêde <121267518+saulocantanhede@users.noreply.github.com>
2025-08-10 17:02:33 -0400

    Update in UBSGNT5 features

---
+++ tf/0.0.2/otext.tf
@@ -0,0 +1,9 @@
+@config
+@dateWritten=2025-06-15T23:17:26Z
+@fmt:text-orig-full={surface}
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@writtenBy=Text-Fabric
+@writtenBy=Text-Fabric
+@dateWritten=2025-08-10T20:14:47Z
+
