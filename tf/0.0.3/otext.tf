0dc92f6bd23eb9f2bab2f44fb1ebd7940e721585
Saulo de Oliveira Cantanhêde <121267518+saulocantanhede@users.noreply.github.com>
2025-08-21 09:25:26 -0400

    Updating and creating new features

---
+++ tf/0.0.3/otext.tf
@@ -0,0 +1,9 @@
+@config
+@dateWritten=2025-06-15T23:17:26Z
+@fmt:text-orig-full={surface}{after}
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@writtenBy=Text-Fabric
+@writtenBy=Text-Fabric
+@dateWritten=2025-08-21T13:07:31Z
+
