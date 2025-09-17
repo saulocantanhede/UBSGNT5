78cae07be74e060f1d200f350d988a73c8bce627
Saulo de Oliveira Cantanhêde <121267518+saulocantanhede@users.noreply.github.com>
2025-09-17 14:59:28 -0400

    Updating and adding features

---
+++ tf/0.0.4/otext.tf
@@ -0,0 +1,10 @@
+@config
+@dateWritten=2025-08-21T13:07:31Z
+@fmt:text-orig-full={before}{text}{after}
+@fmt:text-orig-plain={text}{trailer}
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@writtenBy=Text-Fabric
+@writtenBy=Text-Fabric
+@dateWritten=2025-09-16T03:40:23Z
+
---
+++ tf/0.0.4/otype.tf
@@ -0,0 +1,19 @@
+@node
+@dateWritten=2025-08-21T13:07:25Z
+@valueType=str
+@writtenBy=Text-Fabric
+@writtenBy=Text-Fabric
+@dateWritten=2025-09-16T03:40:17Z
+
+1-138013	word
+138014-138040	book
+138041-138300	chapter
+138301-194557	clause
+194558-270709	phrase
+270710-271567	functionalword
+271568-279508	parsedtext
+279509-422610	subphrase
+422611-423478	section
+423479-431419	sentence
+431420-439360	tree
+439361-447301	verse
