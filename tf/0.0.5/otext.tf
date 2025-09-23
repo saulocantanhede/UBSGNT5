a93f6bc3cada9140c45e365ff6b3839e223c3c41
Saulo de Oliveira Cantanhêde <121267518+saulocantanhede@users.noreply.github.com>
2025-09-22 21:14:25 -0400

    Updating features and adding edge features

---
+++ tf/0.0.5/otext.tf
@@ -0,0 +1,10 @@
+@config
+@dateWritten=2025-09-22T16:59:19Z
+@fmt:text-orig-full={before}{text}{after}
+@fmt:text-orig-plain={text}{trailer}
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@writtenBy=Text-Fabric
+@writtenBy=Text-Fabric
+@dateWritten=2025-09-23T00:50:43Z
+
