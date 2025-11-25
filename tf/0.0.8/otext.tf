8b05ec6dd4383c7a9f76b8f2b375c97a30b061fc
Saulo de Oliveira Cantanhêde <saulo.cantanhede@outlook.com>
2025-11-25 00:18:59 -0500

    fixing description

---
+++ tf/0.0.8/otext.tf
@@ -0,0 +1,10 @@
+@config
+@dateWritten=2025-10-17T19:07:07Z
+@fmt:text-orig-full={before}{text}{after}
+@fmt:text-orig-plain={text}{trailer}
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@writtenBy=Text-Fabric
+@writtenBy=Text-Fabric
+@dateWritten=2025-11-25T04:42:17Z
+
